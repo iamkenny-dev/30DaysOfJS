129763bd4b65cb1bb9aeec887275d3eaaa271196
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-22 17:22:13 +0100

    task1

---
+++ Day 2 Exercises/Exercise Level 2/task1.js
@@ -0,0 +1,4 @@
+// 
+
+let paragraph = 'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.'
+console.log(paragraph);
