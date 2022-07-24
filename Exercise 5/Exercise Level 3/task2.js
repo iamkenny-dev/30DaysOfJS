fe0e4ce988da05348840929646af525af9ef59ef
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-24 19:24:35 +0100

    Exercise: Level 3

---
+++ Exercise 5/Exercise Level 3/task2.js
@@ -0,0 +1,1 @@
+// Find the middle country(ies) in the countries array
---
+++ Exercise 5/Exercise Level 3/task3.js
@@ -0,0 +1,1 @@
+// Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
