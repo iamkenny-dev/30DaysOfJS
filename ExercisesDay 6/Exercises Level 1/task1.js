6b0e23de0465da15561d6c6c376bab503d57124d
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-24 19:52:03 +0100

    Exercises: Level 1

---
+++ ExercisesDay 6/Exercises Level 1/task1.js
@@ -0,0 +1,30 @@
+// Iterate 0 to 10 using for loop, do the same using while and do while loop
+//
+// const countries = [
+//   'Albania',
+//     'Bolivia',
+//       'Canada',
+//         'Denmark',
+//           'Ethiopia',
+//             'Finland',
+//               'Germany',
+//                 'Hungary',
+//                   'Ireland',
+//                     'Japan',
+//                       'Kenya'
+//                       ]
+//
+//                       const webTechs = [
+//                         'HTML',
+//                           'CSS',
+//                             'JavaScript',
+//                               'React',
+//                                 'Redux',
+//                                   'Node',
+//                                     'MongoDB'
+//                                     ]
+//
+//                                     const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+//
+
+
