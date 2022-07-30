a0b8440249a6ec238d33691628e8316c9c398707
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-30 17:57:55 +0100

    my comits

--- ExercisesDay 6/Exercises Level 1/task4.js
+++ ExercisesDay 6/Exercises Level 1/task4.js
@@ -1,12 +1,11 @@
 // Write a loop that makes the following pattern using console.log():
+
 let i = '#', n = '7#';
 while (i <= n) {
-	    if (i <= '7#') {
-		        console.log(i);
-		        console.log('\n');
-		        i = i + '#';
-		        }
-	    else {
-		            break;
-		        }
+	           
+	    console.log(i);
+	    i = i + '#';
+	    
+	                        
+	        
 }
--- ExercisesDay 6/Exercises Level 1/task6.js
+++ ExercisesDay 6/Exercises Level 1/task6.js
@@ -1,9 +1,8 @@
 // Using loop print the following pattern
 
 let i = 0;
-console.log(`i   i^2   i^3`);
+console.log(`i   i^2 i^3`);
 while (i <= 10) {
-	    console.log(`${i}   ${i}^2   ${i}^3`);
-	    console.log('\n');
-	    i++;
+	            console.log(`${i}   ${i**2}   ${i**3}`);
+	            i++;
 }
