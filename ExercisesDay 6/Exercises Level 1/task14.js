53806e33d2466f0290368f7a821f6eabbd39d8ba
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-30 18:32:21 +0100

    my comits

--- ExercisesDay 6/Exercises Level 1/task14.js
+++ ExercisesDay 6/Exercises Level 1/task14.js
@@ -1,9 +1,16 @@
 // Develop a small script which generate array of 5 random numbers and the numbers must be unique
 
 let array = [];
-
+var i;
 for (i = 1; i <= 5; i++) {
-	    let x = Math.floor(Math.random() * 10)
-	   array.push(x);
+	            let x = Math.floor(Math.random() * 11);
+	            if (array.includes(x)) {
+			                    i--;
+			                    continue;
+			                }
+	            else {
+			                array.push(x);
+			                }
 }
 console.log(array);
+
