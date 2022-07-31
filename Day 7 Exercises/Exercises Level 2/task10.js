924d228302b99fae0b77c31626d432788e4f43a5
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-31 20:19:17 +0100

    sum of numbers

---
+++ Day 7 Exercises/Exercises Level 2/task10.js
@@ -0,0 +1,7 @@
+// Declare a function name sumOfNumbers. It takes a number parameter and it adds all the numbers in that range
+//
+// function sumOfNumbers(x, y, z) {
+//     let sum = x + y + z;
+//         console.log(sum);
+//         }
+//         sumOfNumbers(5, 6, 7);
---
+++ Day 7 Exercises/Exercises Level 2/task9.js
@@ -0,0 +1,6 @@
+// Declare a function name removeItem. It takes an index parameter and it returns an array after removing an item
+
+function removeItem(x) {
+	    let remove = 
+		    console.log()
+}
