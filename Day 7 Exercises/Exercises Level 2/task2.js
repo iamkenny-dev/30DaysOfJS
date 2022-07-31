244e907cae5a82c83a7660883925ef7ea4a89df9
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-31 13:58:43 +0100

    solveQuadEquation

---
+++ Day 7 Exercises/Exercises Level 2/task2.js
@@ -0,0 +1,15 @@
+// Quadratic equation is calculated as follows: ax2 + bx + c = 0. Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
+//
+// console.log(solveQuadratic()) // {0}
+// console.log(solveQuadratic(1, 4, 4)) // {-2}
+// console.log(solveQuadratic(1, -1, -2)) // {2, -1}
+// console.log(solveQuadratic(1, 7, 12)) // {-3, -4}
+// console.log(solveQuadratic(1, 0, -4)) //{2, -2}
+// console.log(solveQuadratic(1, -1, 0)) //{1, 0}
+
+function solveQuadEquation(a, b, c) {
+	    console.log(`${a}(x**2) + ${b}x + ${c} = 0`);
+}
+solveQuadEquation(1, -1, 0)
+
+
