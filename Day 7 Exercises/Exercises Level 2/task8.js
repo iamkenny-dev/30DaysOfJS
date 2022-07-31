9ed970c1fb75f721ed158ea3058249e672147e28
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-31 19:43:35 +0100

    add sum

---
+++ Day 7 Exercises/Exercises Level 2/task8.js
@@ -0,0 +1,7 @@
+// Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
+
+function addItem(x, y) {
+	    let sum = x + y;
+	    console.log([sum]);
+}
+addItem(1, 2)
