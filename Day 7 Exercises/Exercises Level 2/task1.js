1c309977177e0c29eaa40a32b15463afb6c0c272
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-31 13:17:23 +0100

    calculates value of a linear equation

---
+++ Day 7 Exercises/Exercises Level 2/task1.js
@@ -0,0 +1,6 @@
+// Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
+
+function solveLinEquation(x, y) {
+	    console.log(`${x}a + ${y}b + c = 0`);
+}
+solveLinEquation(1, 5);
