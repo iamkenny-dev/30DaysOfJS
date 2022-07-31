71bbc1a63fd4f2880e6a228bb3d4ffd86c7180fb
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-31 10:17:00 +0100

    my commits

--- Day 7 Exercises/Exercises Level 1/task15.js
+++ Day 7 Exercises/Exercises Level 1/task15.js
@@ -1,8 +1,13 @@
-// Math.max returns its largest argument. Write a function findMax that takes three arguments and returns their maximum with out using Math.max method.
+Math.max returns its largest argument. Write a function findMax that takes three arguments and returns their maximum with out using Math.max method.
 //
 // console.log(findMax(0, 10, 5))
 // 10
 // console.log(findMax(0, -10, -2))
 // 0
 
+// Math.max returns 
 
+function findMax(x, y, z) {
+	console.log(Math.max(x, y, z))
+}
+findMax(1, 2, 3);
