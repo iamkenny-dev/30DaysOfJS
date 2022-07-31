ba2dce045cba71147e339d0275f39445d3d10ffa
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-31 14:14:39 +0100

    print array

---
+++ Day 7 Exercises/Exercises Level 2/task3.js
@@ -0,0 +1,6 @@
+// Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+
+function printArray(...arr) {
+	    console.log(arr)
+}
+printArray(1, 3, 5);
