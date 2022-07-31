5a6555ef5dfa2cd96978a2af54dbdced83abf409
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-07-31 19:21:10 +0100

    to uppercase

---
+++ Day 7 Exercises/Exercises Level 2/task6.js
@@ -0,0 +1,14 @@
+// function swapValues(x, y) {
+//     let swap = x;
+//         x = y;
+//             y = swap;
+//                 console.log(x, y)
+//                 }
+//                 swapValues(1, 2)
+
+function reverseArray(...arr) {
+	    let array = arr.reverse()
+	    console.log(array);
+}
+
+reverseArray('A', 'B', 'C')
---
+++ Day 7 Exercises/Exercises Level 2/task7.js
@@ -0,0 +1,8 @@
+// Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+function capitalizeArray(...arr) {
+	    let capital = arr.toString().toUpperCase();
+	    
+	    console.log(capital.split(','))
+}
+capitalizeArray('r', 't');
