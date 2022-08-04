d7e9ed335344cf0d27b0597134bc5e93caa88237
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-08-04 14:01:25 +0100

    my comits

--- Day 7 Exercises/Exercises Level 3/task2.js
+++ Day 7 Exercises/Exercises Level 3/task2.js
@@ -2,3 +2,14 @@
 //
 // rgbColorGenerator()
 // rgb(125,244,255)
+
+
+function rgbColorGenerator() {
+	let x = Math.floor(Math.random() * 256);
+	let y = Math.floor(Math.random() * 256);
+	let z = Math.floor(Math.random() * 256);
+
+	console.log(`rgb(${x}, ${y}, ${z})`);
+}
+
+rgbColorGenerator()
