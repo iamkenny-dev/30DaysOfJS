95f42858ed561e7d263533487253cb3e4e0cff36
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-08-04 21:05:44 +0100

    my comits

--- ExercisesDay 8/Exercises Level 2/task1.js
+++ ExercisesDay 8/Exercises Level 2/task1.js
@@ -1,2 +1,20 @@
 // Find the person who has many skills in the users object.
 
+console.log(users.Alex.skills.length);
+console.log(users.Paul.skills.length)
+// let alexSkillsNum = users.Alex.skills.length;
+// // let asabSkillsNum = users.Asab.skills.length
+// // let brookSkillsNum = users.Brook.skills.length;
+// // let danielSkillsNum = users.Daniel.skills.length;
+// // let johnSkillsNum = users.John.skills.length;
+// // let thomasSkillsNum = users.Thomas.skills.length;
+// // let paulSkillsNum = users.Paul.skills.length;
+//
+// // for (i = 0; i < 7; i++) {
+// //     let longestNumber;
+// //     let length = 0;
+// //     if ()
+// // }
+//
+// const keys = Object.keys(users);
+// console.log(users.keys);
--- ExercisesDay 8/Exercises Level 2/task2.js
+++ ExercisesDay 8/Exercises Level 2/task2.js
@@ -1,1 +1,3 @@
 // Count logged in users, count users having greater than equal to 50 points from the following object.
+
+
