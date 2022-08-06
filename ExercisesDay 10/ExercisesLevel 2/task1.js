d8cf2fef449fea8c1c1719b04214f771b9c672f8
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-08-06 23:18:34 +0100

    my comits

---
+++ ExercisesDay 10/ExercisesLevel 2/task1.js
@@ -0,0 +1,12 @@
+// Find a union b
+
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7]
+// const countries = ['Finland', 'Sweden', 'Norway']
+// const c = [...a, ...b]
+//
+// let A = new Set(a)
+// let B = new Set(b)
+// let C = new Set(c)
+//
+// console.log(C)
---
+++ ExercisesDay 10/ExercisesLevel 2/task3.js
@@ -0,0 +1,14 @@
+// Find a with b
+
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7]
+// const countries = ['Finland', 'Sweden', 'Norway']
+//
+//
+// let A = new Set(a)
+// let B = new Set(b)
+// let C = new Set(c)
+//
+// const c = a.filter((num) => !B.has(num))
+// console.log(C)
+//
