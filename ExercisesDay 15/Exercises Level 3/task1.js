995d82a8c01b1e9eb8d09792612d2b9dbd3372ea
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-08-07 18:32:24 +0100

    my comits

---
+++ ExercisesDay 15/Exercises Level 3/task1.js
@@ -0,0 +1,3 @@
+// Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+
