f41f6777373c3d8b88e30795e9a3c1ac3b3fb0d6
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-08-07 11:08:35 +0100

    my comits

---
+++ ExercisesDay 12/Exercises Level 3/task1.js
@@ -0,0 +1,5 @@
+// Writ a function which cleans text. Clean the following text. After cleaning, count three most frequent words in the string.
+
+const pattern = /[^a-zA-Z]/g
+const cleanTextSentence = sentence.replace(pattern, '')
+  console.log(cleanTextSentence)
---
+++ ExercisesDay 12/Exercises Level 3/task2.js
@@ -0,0 +1,3 @@
+// Write a function which find the most frequent words. After cleaning, count three most frequent words in the string.
+
+
